89414d505f349262cd0d911e1090dbf722f3d5b1
FrancisUzoigwe <kossyuzoigwe@gmail.com>
2024-01-30 01:09:14 +0100

    little changes

--- dist/router/productRouter.js
+++ dist/router/productRouter.js
@@ -8,7 +8,7 @@ const productController_1 = require("../controller/productController");
 const multer_1 = __importDefault(require("multer"));
 const upload = (0, multer_1.default)().single("image");
 const router = (0, express_1.Router)();
-router.route("/:userID/create-product").post(upload, productController_1.createProduct);
+router.route("/create-product").post(upload, productController_1.createProduct);
 router.route("/:productID/delete-product").delete(productController_1.deleteProduct);
 router.route("/view-all-products").get(productController_1.viewAllProduct);
 router.route("/:sellerID/populate").get(productController_1.populateProduct);
